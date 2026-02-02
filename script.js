a038bd4833e6dba1966df13b2dff224519058e6f
GABRIEL JOSÉ SANTOS DA SILVA <25132745@CFP127ALUNO.LOCAL>
2026-02-02 16:57:51 -0300

    Quase tudo feito

---
+++ script.js
@@ -0,0 +1,42 @@
+'use strict'
+
+//Criando as variaveis dos elementos
+const botaoTrocarCor = document.getElementById('botao-trocar-cor');
+const botaoTrocarCorVerde = document.getElementById('botao-verde');
+const botaoTrocarCorVermelho = document.getElementById('botao-vermelho');
+
+function arcoIris() {
+    setTimeout()
+}
+
+//Criando uma função que obtem o valor do campo e muda a variavel --background-color para esse valor
+function trocarCor() {
+    const corUsuario = document.getElementById('cor-usuario').value;
+
+    if (corUsuario == 'verde') {
+        document.documentElement.style.setProperty('--background-color', 'green');
+    } else if (corUsuario == 'azul') {
+        document.documentElement.style.setProperty('--background-color', 'blue');
+    } else if (corUsuario == 'rosa') {
+        document.documentElement.style.setProperty('--background-color', 'pink');
+    } else if (corUsuario == 'marrom') {
+        document.documentElement.style.setProperty('--background-color', 'brown');
+    } else if(corUsuario == 'arco-iris') {
+
+    } else {
+        document.documentElement.style.setProperty('--background-color', corUsuario);
+    }
+}
+
+function trocaCorVerde() {
+    document.documentElement.style.setProperty('--background-color', 'green');
+}
+
+function trocaCorVermelho() {
+    document.documentElement.style.setProperty('--background-color', 'red');
+}
+
+//Caso o botão seja pressionado ele chama a função troca cor
+botaoTrocarCor.addEventListener('click', trocarCor);
+botaoTrocarCorVerde.addEventListener('click', trocaCorVerde);
+botaoTrocarCorVermelho.addEventListener('click', trocaCorVermelho);
